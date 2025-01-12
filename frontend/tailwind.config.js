741ba20c8b0e8d245251d5aaa5324414c75518fd
Aditya Tiwari <mycoding2025@gmail.com>
2025-01-12 16:55:51 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+const daisyui = await import("daisyui");
+
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
 	// eslint-disable-next-line no-undef
-	plugins: [daisyui],
+	plugins: [daisyui.default],
 };
